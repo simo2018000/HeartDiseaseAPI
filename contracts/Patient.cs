fbf1967c2604b0d4454c19032495602be007a593
nihi_l14 <simobatar.12@gmail.com>
2025-05-12 00:33:08 +0100

    Add project files.

---
+++ contracts/Patient.cs
@@ -0,0 +1,21 @@
+namespace HeartDiseaseAPI.contracts
+{
+    public class Patient
+    {
+        public int ID { get; set; }
+        public int Age { get; set; } // in days 
+        public string Sex { get; set; }
+        public int Height { get; set; } // in cm
+        public int Weight { get; set; } // in kg    
+        public float BloodPressureLow { get; set; }
+        public float BloodPressureHigh { get; set; }
+        public float Cholesterol { get; set; }
+        public float Glucose { get; set; }
+        public bool IsSmoker { get; set; }
+        public bool IsAlcoholic { get; set; }
+        public bool IsActive { get; set; }
+        public bool HasHeartDisease { get; set; }
+
+    }
+
+}
