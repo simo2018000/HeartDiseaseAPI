172e806725ec89443b3e7e147e8000853df734ca
nihi_l14 <simobatar.12@gmail.com>
2025-05-12 01:35:08 +0100

    commit all

---
+++ Mapping/AutoMapperProlfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HeartDiseaseAPI.Dtos;
+
+
+namespace HeartDiseaseAPI.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Patient, PatientReadDto>();
+            CreateMap<PatientCreateDto, Patient>();
+        }
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace HeartDiseaseAPI.Models
+{
+    public class Patient
+    {
+        public int ID { get; set; }
+        public int Age { get; set; }// in days
+        public String Sex { get; set; } = string.Empty;
+        public int Height { get; set; } // in cm
+        public int Weight { get; set; } // in kg
+        public float BloodPressureLow { get; set; } // in mmHg
+        public float BloodPressureHigh { get; set; } // in mmHg
+        public float Cholesterol { get; set; } // in mg/dL
+        public bool IsSmoker { get; set; }
+        public bool IsAlcoholic { get; set; }
+        public bool IsActive { get; set; }
+        public bool HasHeartDisease { get; set; }
+
+    }
+}
